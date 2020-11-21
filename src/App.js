8858985f45d517b7d32513c8ea486edc2de5eec0
Fernando <fercanepari@gmail.com>
2020-11-21 04:04:17 -0300

    Estilos agregados al login

--- src/App.js
+++ src/App.js
@@ -1,33 +1,47 @@
 import React from 'react';
-import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import {Navbar, Nav} from 'react-bootstrap'
-import Login from './components/Login'
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {Nav} from 'react-bootstrap'
+import Login from "./components/login.component";
+//import SignUp from "./components/signup.component";
 import Farmacia from './Farmacia'
-import About from './components/About'
-import {Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 function App() {
-
-  return (
+  return (<Router>
     <div className="App">
-      <Router>
-        <Navbar bg="primary" variant="dark">
-          <Navbar.Brand href="#home">Gestion Promociones</Navbar.Brand>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+
+          <Link className="navbar-brand" to={"/sign-in"}>Gestion Pedidos</Link>
           <Nav className="mr-auto">
-            <Nav.Link><Link to="/login">Login</Link></Nav.Link>
             <Nav.Link><Link to="/farmacia">Farmacia</Link></Nav.Link>
             <Nav.Link><Link to="/about">About</Link></Nav.Link>
           </Nav>
-        </Navbar>
-        <Switch>
-        <Route path="/login" component={Login} ></Route>
-        <Route path="/farmacia" component={Farmacia} ></Route>
-        <Route path="/about" component={About} ></Route>
-        <Route path="/" component={Login} ></Route>
-        </Switch>
-      </Router>
-    </div>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+          </Switch>
+        </div>
+        <div>
+          <Switch>
+            <Route path="/farmacia" component={Farmacia} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
   );
 }
 
---
+++ src/components/login.component.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          name: "",
+          password: "",
+          loginErrors: ""
+        };
+    
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+      }
+    
+      handleChange(event) {
+        this.setState({
+          [event.target.name]: event.target.value
+        });
+      }
+    
+      handleSubmit(event) {
+        const { name, password } = this.state;
+    
+        var url = 'http://www.kellerhoff.com.ar:84/api/';
+        var data = {};
+        data.login = name;
+        data.pass = password;
+        var json = JSON.stringify(data);
+        fetch(url + 'Authenticate', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: json
+        })
+          .then(results => results.json())
+          .then(data => {
+            // const {name} = data.results[0];
+            //setFirstName(name.first);
+            alert('OK' + data.apNombre);
+            //  setLastName(name.last);
+    
+          });
+      }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h3>Inicio sesion</h3>
+
+                <div className="form-group">
+                    <label>Usuario</label>
+                    <input type="text" name="user" className="form-control" placeholder="Ingrese usuario" value={this.state.user}
+                        onChange={this.handleChange} required/>
+                </div>
+
+                <div className="form-group">
+                    <label>Contraseña</label>
+                    <input type="password" name="password" className="form-control" placeholder="Ingrese contraseña" value={this.state.password}
+                        onChange={this.handleChange} required />
+                </div>
+
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    </div>
+                </div>
+
+                <button type="submit" className="btn btn-primary btn-block">Login</button>
+               
+            </form>
+        );
+    }
+}
